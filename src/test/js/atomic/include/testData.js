00cf1db6f75c36507713c8f51af9bebb899e16e4
techtangents <dev@ephox.com>
2011-06-22 20:25:30 +1000

    Testing

--- src/test/js/atomic/include/testData.js
+++ src/test/js/atomic/include/testData.js
@@ -16,7 +16,7 @@ var testValues = [null, undefined, [], {}, 0, -1, 1, 2, [1], "a", "", "b", funct
 var testArrays = testValues.map(arrayUnital).concat([
     [], [[]], [[[]]], [[[[]]]], [1, [2], [[3]]], [1, 2, 3, 4], testValues, ['a', 'b', 'c', 'd']
 ]);
-
+var testInts = [-1000, -50, -49, -100, -10, -3, -2, -1, 0, 1, 2, 3, 4, 10, 11, 99, 100, 1000, 10000, 1012413];
 
 var permute2 = function(input, f) {
     input.forEach(function(a) {
--- src/test/js/atomic/tests/Future/FutureTest.js
+++ src/test/js/atomic/tests/Future/FutureTest.js
@@ -1,33 +1,37 @@
 require("../../include/include.js");
 
-// TODO: this drives out a varargs bind, e.g. futureA.bindn(sz, suffixQ), or perhaps >>= could just take varargs?
 var testBind = function() {
-    withSpy(function() {
-        return plus1(5) [">>="] (sz) [">>="] (suffixQ);
-    }, [["6q"]]);
+    testInts.forEach(function(x) {
+        withSpy(function() {
+            return plus1(x) [">>="] (sz) [">>="] (suffixQ);
+        }, [[String(x + 1) + "q"]]);
+    });
 };
 
 var testAnonBind = function() {
-    var spy1 = jssert.spy();
-    var spy2 = jssert.spy();
+    permute2(testValues, function(x, y) {
+        var spy1 = jssert.spy();
+        var spy2 = jssert.spy();
 
-    var futureA = Future.future(function(callback) {
-        spy1();
-        callback(3);
+        var futureA = Future.future(function(callback) {
+            spy1();
+            callback(x);
+        });
+        var futureB = Future.constant(y);
+        var f = (futureA) [">>"] (futureB);
+        f(spy2);
+
+        jssert.assertEq(1, spy1.getInvocations().length);
+        spy2.verifyArgs([[y]]);
     });
-    var futureB = Future.constant(4);
-    var f = (futureA) [">>"] (futureB);
-    f(spy2);
-
-    jssert.assertEq(1, spy1.getInvocations().length);
-    spy2.verifyArgs([[4]]);
 };
 
 var testMap = function() {
-    var spy = jssert.spy();
-    var futureA = Future.constant(3) ["<$>"] (function(x) { return x + 2; });
-    futureA(spy);
-    spy.verifyArgs([[5]]);
+    testValues.forEach(function(v) {
+        withSpy(function() {
+            return Future.constant(v) ["<$>"] (function(x) { return x + 2; })
+        }, [[v + 2]]);
+    });
 };
 
 var testConstant = function() {
