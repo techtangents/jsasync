58b8c4e8576961d345d4f2855a22ba4366667d53
techtangents <dev@ephox.com>
2011-06-25 20:46:55 +1000

    Refactoring tests

--- src/test/js/atomic/tests/Bsync/BsyncTest.js
+++ src/test/js/atomic/tests/Bsync/BsyncTest.js
@@ -1,52 +1,43 @@
 require("../../include/include.js");
 
 function testIdentity() {
-    testValues.forEach(function(x) {
-        var spy = jssert.spy();
+    forEachWithSpy(testValues, function(x, spy) {
         Bsync.identity(x)(spy, explode);
         spy.verifyArgs([[x]]);
     });
 }
 
 function testFaildentity() {
-    testValues.forEach(function(x) {
-        var spy = jssert.spy();
+    forEachWithSpy(testValues, function(x, spy) {
         Bsync.faildentity(x)(explode, spy);
         spy.verifyArgs([[x]]);
     });
 }
 
 function testPass() {
-    testFunctionsFromInt.forEach(function(f) {
-        testInts.forEach(function(input) {
-            var bs = Bsync.bsync(function(a, passCb, failCb) {
-                passCb(f(a));
-            });
-            var spy = jssert.spy();
-            bs(input)(spy, explode);
-            var expected = f(input)
-            spy.verifyArgs([[expected]]);
+    forEach2WithSpy(testFunctionsFromInt, testInts, function(f, input, spy) {
+        var bs = Bsync.bsync(function(a, passCb, failCb) {
+            passCb(f(a));
         });
+        bs(input)(spy, explode);
+        var expected = f(input)
+        spy.verifyArgs([[expected]]);
    });
 }
 
 function testFail() {
-    testFunctionsFromInt.forEach(function(f) {
-        testInts.forEach(function(input) {
-            var bs = Bsync.bsync(function(a, passCb, failCb) {
-                failCb(f(a));
-            });
-            var spy = jssert.spy();
-            bs(input)(explode, spy);
-            var expected = f(input)
-            spy.verifyArgs([[expected]]);
+    forEach2WithSpy(testFunctionsFromInt, testInts, function(f, input, spy) {
+        var bs = Bsync.bsync(function(a, passCb, failCb) {
+            failCb(f(a));
         });
+        bs(input)(explode, spy);
+        var expected = f(input);
+        spy.verifyArgs([[expected]]);
    });
 }
 
 function testBsyncConstantFail() {
-    testInts.forEach(function(input) {
-        var spy = jssert.spy();
+    forEachWithSpy(testInts, function(input, spy) {
         Bsync.constantFail("no dice")("ignored")(explode, spy);
         spy.verifyArgs([["no dice"]]);
     });
