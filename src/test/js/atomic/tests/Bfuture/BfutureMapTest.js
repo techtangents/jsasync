f6ac75aa5f1c5ce3d034d7cd256197d56f9fb5f2
techtangents <dev@ephox.com>
2011-06-26 18:57:56 +1000

    testing

--- src/test/js/atomic/tests/Bfuture/BfutureMapTest.js
+++ src/test/js/atomic/tests/Bfuture/BfutureMapTest.js
@@ -1,12 +1,6 @@
 require("../../include/include.js");
 
-
-var testMap = function() {
-    var input = 3;
-    var mapper = function(x) { return x + 1; };
-
-    var spy = jssert.spy();
-
-    Bfuture.constant(input).map(mapper)(spy, explode);
-    spy.verifyArgs([[mapper(input)]]);
-};
+var testMap = forEach2WithSpy_(testFunctionsFromInt, testInts, function(f, input, spy) {
+    Bfuture.constant(input).map(f)(spy, explode);
+    spy.verifyArgs([[f(input)]]);
+});
