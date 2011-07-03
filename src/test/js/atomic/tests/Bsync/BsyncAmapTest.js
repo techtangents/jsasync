44ccc46135d27aa9b296a0dd29dffdbc451d7f84
techtangents <dev@ephox.com>
2011-07-03 12:41:26 +1000

    Test for Bsync.amap

--- src/test/js/atomic/tests/Bsync/BsyncAmapTest.js
+++ src/test/js/atomic/tests/Bsync/BsyncAmapTest.js
@@ -6,3 +6,20 @@ var testPass = forEach2WithSpy_(testFunctions, testArrays, function(f, inputs, s
     bs.amap(inputs)(spy, explode);
     spy.verifyArgs([[expected]]);
 });
+
+var testFail = forEachWithSpy_(testInts, function(inputs_, spy) {
+    var inputs = [1].concat(inputs_); // so there's always a fail
+
+    var spy = jssert.spy();
+    var pred = function(a) { return a >= 2; };
+
+    var expected = inputs.map(function(a) {
+        return (pred(a) ? Either.good : Either.bad)(a);
+    });
+    var bs = Bsync.bsync(function(a, passCb, failCb) {
+         pred(a) ? passCb(a) : failCb(a);
+    });
+    bs.amap(inputs)(explode, spy);
+    var actual = spy.getInvocationArgs()[0][0];
+    assertArrayOfEitherEquals(expected, actual);
+});
