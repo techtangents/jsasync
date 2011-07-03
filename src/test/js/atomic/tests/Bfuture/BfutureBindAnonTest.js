89b5af668e6c04c197ea0f31307c85470c7cfb40
techtangents <dev@ephox.com>
2011-07-04 07:47:18 +1000

    Test for Bfuture >>

--- src/test/js/atomic/tests/Bfuture/BfutureBindAnonTest.js
+++ src/test/js/atomic/tests/Bfuture/BfutureBindAnonTest.js
@@ -4,3 +4,16 @@ var testPassPassSimple = forEach2WithSpy_(testValues, testValues, function(x, y,
     Bfuture.constant(x)[">>"](Bfuture.constant(y))(spy, explode);
     spy.verifyArgs([[y]]);
 });
+
+var testPassPass2 = function() {
+    testValues.forEach(function(x) {
+        var spy1 = jssert.spy();
+        var spy2 = jssert.spy();
+        Bfuture.bfuture(function(passCb, failCb) {
+            spy1();
+            passCb();
+        })[">>"](Bfuture.constant(x))(spy2, explode);
+        spy1.verifyArgs([[]]);
+        spy2.verifyArgs([[x]]);
+    });
+};
