d5c3621b2c7f7a7c167e230a9427dde20080bee9
techtangents <dev@ephox.com>
2011-07-07 19:56:52 +1000

    testing mapIn

---
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncMapInTest.js
@@ -0,0 +1,20 @@
+require("../../../include/include.js");
+
+var test = forEach2_(testInts, testFunctionsFromInt, function(input, f) {
+    function check(q, expected) {
+        var spy = jssert.spy();
+        q(spy, explode);
+        spy.verifyArgs([[expected]]);
+    }
+
+    var id = function(x) { return x; };
+
+    // input -> f -> identity = f(input)
+    check(Bsync.identity.mapIn(f)(input), f(input));
+
+    // input -> identity -> f = f(input)
+    check(Bsync.sync(f).mapIn(id)(input), f(input));
+
+    // input -> identity -> identity -> input
+    check(Bsync.sync(id).mapIn(id)(input), input);
+});
