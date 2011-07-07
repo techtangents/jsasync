11eed250548612406eb9323432687cf5bf2c1889
techtangents <dev@ephox.com>
2011-07-07 20:14:43 +1000

    Tests for mapInAsync

--- src/test/js/atomic/tests/stacked/Bsync/BsyncMapInTest.js
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncMapInTest.js
@@ -11,13 +11,17 @@ var test = forEach2_(testInts, testFunctionsFromInt, function(input, f) {
 
     // input -> f -> identity = f(input)
     check(Bsync.identity.mapIn(f)(input), f(input));
+    check(Bsync.identity.mapInAsync(Async.sync(f))(input), f(input));
 
     // input -> identity -> f = f(input)
     check(Bsync.sync(f).mapIn(id)(input), f(input));
+    check(Bsync.sync(f).mapInAsync(Async.sync(id))(input), f(input));
 
     // input -> identity -> identity -> input
     check(Bsync.sync(id).mapIn(id)(input), input);
+    check(Bsync.sync(id).mapInAsync(Async.sync(id))(input), input);
 
     // input -> f -> f -> f(f(input)
     check(Bsync.sync(f).mapIn(f)(input), f(f(input)));
+    check(Bsync.sync(f).mapInAsync(Async.sync(f))(input), f(f(input)));
 });
