b939da92d1943acd3870a1d3a59ebf06130700ef
techtangents <dev@ephox.com>
2011-07-08 12:31:14 +1000

    Refactoring tests

--- src/test/js/atomic/tests/stacked/Bsync/BsyncMapFailTest.js
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncMapFailTest.js
@@ -4,14 +4,10 @@ var fnNames = ["mapFail", "<!>"];
 
 var testMapFailOnFail = forEach3_(testFunctionsFromInt, testInts, testValues, function(f, input, dummy) {
     fnNames.forEach(function(fnName) {
-        var spy = jssert.spy();
-        Bsync.constantFail(input)[fnName](f)(dummy)(explode, spy);
-        spy.verifyArgs([[f(input)]]);
+        checkBfFail(Bsync.constantFail(input)[fnName](f)(dummy), f(input));
     });
 });
 
 var testMapFailOnPass = forEach3_(testInts, testValues, fnNames, function(input, dummy, fnName) {
-    var spy = jssert.spy();
-    Bsync.constant(input)[fnName](explode)(dummy)(spy, explode);
-    spy.verifyArgs([[input]]);
+    checkBfPass(Bsync.constant(input)[fnName](explode)(dummy), input);
 });
--- src/test/js/atomic/tests/stacked/Bsync/BsyncMapInTest.js
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncMapInTest.js
@@ -1,27 +1,22 @@
 require("../../../include/include.js");
 
 var test = forEach2_(testInts, testFunctionsFromInt, function(input, f) {
-    function check(q, expected) {
-        var spy = jssert.spy();
-        q(spy, explode);
-        spy.verifyArgs([[expected]]);
-    }
 
     var id = function(x) { return x; };
 
     // input -> f -> identity = f(input)
-    check(Bsync.identity.mapIn(f)(input), f(input));
-    check(Bsync.identity.mapInAsync(Async.sync(f))(input), f(input));
+    checkBfPass(Bsync.identity.mapIn(f)(input), f(input));
+    checkBfPass(Bsync.identity.mapInAsync(Async.sync(f))(input), f(input));
 
     // input -> identity -> f = f(input)
-    check(Bsync.sync(f).mapIn(id)(input), f(input));
-    check(Bsync.sync(f).mapInAsync(Async.sync(id))(input), f(input));
+    checkBfPass(Bsync.sync(f).mapIn(id)(input), f(input));
+    checkBfPass(Bsync.sync(f).mapInAsync(Async.sync(id))(input), f(input));
 
     // input -> identity -> identity -> input
-    check(Bsync.sync(id).mapIn(id)(input), input);
-    check(Bsync.sync(id).mapInAsync(Async.sync(id))(input), input);
+    checkBfPass(Bsync.sync(id).mapIn(id)(input), input);
+    checkBfPass(Bsync.sync(id).mapInAsync(Async.sync(id))(input), input);
 
     // input -> f -> f -> f(f(input)
-    check(Bsync.sync(f).mapIn(f)(input), f(f(input)));
-    check(Bsync.sync(f).mapInAsync(Async.sync(f))(input), f(f(input)));
+    checkBfPass(Bsync.sync(f).mapIn(f)(input), f(f(input)));
+    checkBfPass(Bsync.sync(f).mapInAsync(Async.sync(f))(input), f(f(input)));
 });
