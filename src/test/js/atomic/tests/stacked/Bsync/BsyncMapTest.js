e83dd3937d2f2599120fd2fa44fce6458cd6821b
techtangents <dev@ephox.com>
2011-07-08 13:02:52 +1000

    Refactoring

--- src/test/js/atomic/tests/stacked/Bsync/BsyncMapTest.js
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncMapTest.js
@@ -6,8 +6,12 @@ var testMapPass = forEach3_(testFunctionsFromInt, testInts, testValues, function
     fnNames.forEach(function(fnName) {
         checkBfPass(Bsync.constant(input)[fnName](f)(dummy), f(input));
     });
+    checkBfPass(Bsync.constant(input).mapAsync(Bsync.sync(f))(dummy), f(input));
 });
 
-var testMapFail = forEach3_(testInts, testValues, fnNames, function(input, dummy, fnName) {
-    checkBfFail(Bsync.constantFail(input)[fnName](explode)(dummy), input);
+var testMapFail = forEach2_(testInts, testValues, function(input, dummy) {
+    fnNames.forEach(function(fnName) {
+        checkBfFail(Bsync.constantFail(input)[fnName](explode)(dummy), input);
+    });
+    checkBfFail(Bsync.constantFail(input).mapAsync(Async.sync(explode))(dummy), input);
 });
