3179559ad3228d285010783644fb2ea42677b7e5
techtangents <dev@ephox.com>
2011-07-08 12:45:56 +1000

    Refactoring tests

--- src/test/js/atomic/tests/stacked/Bfuture/BfutureConstantTest.js
+++ src/test/js/atomic/tests/stacked/Bfuture/BfutureConstantTest.js
@@ -1,11 +1,6 @@
 require("../../../include/include.js");
 
-var testConstant = forEachWithSpy_(testInts, function(input, spy) {
-    Bfuture.constant(input)(spy, explode);
-    spy.verifyArgs([[input]]);
-});
-
-var testConstantFail = forEachWithSpy_(testInts, function(input, spy) {
-    Bfuture.constantFail(input)(explode, spy);
-    spy.verifyArgs([[input]]);
+var test = forEach_(testInts, function(input) {
+    checkBfPass(Bfuture.constant(input), input);
+    checkBfFail(Bfuture.constantFail(input), input);
 });
