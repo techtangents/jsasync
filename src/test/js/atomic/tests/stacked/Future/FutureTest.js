3287633ce7b2834c8ddabeeb682b69842a199c63
techtangents <dev@ephox.com>
2011-07-08 13:21:17 +1000

    Refactoring

--- src/test/js/atomic/tests/stacked/Future/FutureTest.js
+++ src/test/js/atomic/tests/stacked/Future/FutureTest.js
@@ -5,19 +5,21 @@ var testBind = forEach2_(testInts, [">>=", "bind"], function(x, fnName) {
 });
 
 var testAnonBind = permute2_(testValues, function(x, y) {
-    var spy1 = jssert.spy();
-    var spy2 = jssert.spy();
+    [">>", "bindAnon"].forEach(function(fnName) {
+        var spy1 = jssert.spy();
+        var spy2 = jssert.spy();
 
-    var futureA = Future.future(function(callback) {
-        spy1();
-        callback(x);
+        var futureA = Future.future(function(callback) {
+            spy1();
+            callback(x);
+        });
+        var futureB = Future.constant(y);
+        var f = (futureA) [fnName] (futureB);
+        f(spy2);
+
+        jssert.assertEq(1, spy1.getInvocations().length);
+        spy2.verifyArgs([[y]]);
     });
-    var futureB = Future.constant(y);
-    var f = (futureA) [">>"] (futureB);
-    f(spy2);
-
-    jssert.assertEq(1, spy1.getInvocations().length);
-    spy2.verifyArgs([[y]]);
 });
 
 var testMap = forEach2_(testInts, ["<$>", "map"], function(v, fnName) {
