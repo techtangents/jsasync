f89faa7abd9a5e8a3a87103b56c542ede8da2943
techtangents <dev@ephox.com>
2011-07-08 12:28:16 +1000

    Refactoring tests

--- src/test/js/atomic/tests/stacked/Bsync/BsyncMapTest.js
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncMapTest.js
@@ -4,14 +4,10 @@ var fnNames = ["map", ">>^", "<$>"];
 
 var testMapPass = forEach3_(testFunctionsFromInt, testInts, testValues, function(f, input, dummy) {
     fnNames.forEach(function(fnName) {
-        var spy = jssert.spy();
-        Bsync.constant(input)[fnName](f)(dummy)(spy, explode);
-        spy.verifyArgs([[f(input)]]);
+        checkBfPass(Bsync.constant(input)[fnName](f)(dummy), f(input));
     });
 });
 
 var testMapFail = forEach3_(testInts, testValues, fnNames, function(input, dummy, fnName) {
-    var spy = jssert.spy();
-    Bsync.constantFail(input)[fnName](explode)(dummy)(explode, spy);
-    spy.verifyArgs([[input]]);
+    checkBfFail(Bsync.constantFail(input)[fnName](explode)(dummy), input);
 });
--- src/test/js/atomic/tests/stacked/Bsync/BsyncSyncTest.js
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncSyncTest.js
@@ -1,9 +1,6 @@
 require("../../../include/include.js");
 
-var testSync = forEach2WithSpy_(testInts, testFunctionsFromInt, function(input, f, spy) {
+var test = forEach2WithSpy_(testInts, testFunctionsFromInt, function(input, f, spy) {
     checkBfPass(Bsync.sync(f)(input), f(input));
-});
-
-var testSyncFail = forEach2WithSpy_(testInts, testFunctionsFromInt, function(input, f, spy) {
     checkBfFail(Bsync.syncFail(f)(input), f(input));
 });
