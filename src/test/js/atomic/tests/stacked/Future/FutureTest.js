8d3ad8bee388782616eaa687b15a17f76f9f5446
techtangents <dev@ephox.com>
2011-07-10 10:34:28 +1000

    Refactor

--- src/test/js/atomic/tests/stacked/Future/FutureTest.js
+++ src/test/js/atomic/tests/stacked/Future/FutureTest.js
@@ -2,22 +2,20 @@ require("../../../include/include.js");
 
 var test = unitTest(function(Future, Async, Bfuture, Bsync) {
 
-    permute2(testValues, function(x, y) {
-        [">>", "bindAnon"].forEach(function(fnName) {
-            var spy1 = jssert.spy();
-            var spy2 = jssert.spy();
+    forEach3(testValues, testValues, [">>", "bindAnon"], function(x, y, fnName) {
+        var spy1 = jssert.spy();
+        var spy2 = jssert.spy();
 
-            var futureA = Future.future(function(callback) {
-                spy1();
-                callback(x);
-            });
-            var f = futureA[fnName](Future.constant(y))(spy2);
-
-            waitForSpies([spy1, spy2]);
-
-            jssert.assertEq(1, spy1.getInvocations().length);
-            spy2.verifyArgs([[y]]);
+        var futureA = Future.future(function(callback) {
+            spy1();
+            callback(x);
         });
+        var f = futureA[fnName](Future.constant(y))(spy2);
+
+        waitForSpies([spy1, spy2]);
+
+        jssert.assertEq(1, spy1.getInvocations().length);
+        spy2.verifyArgs([[y]]);
     });
 
     forEach2(testInts, [">>=", "bind"], function(x, fnName) {
