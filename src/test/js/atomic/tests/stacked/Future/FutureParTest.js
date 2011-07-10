e7cfbe5b2b12d9d8bf7230603d1f6e021b427e4a
techtangents <dev@ephox.com>
2011-07-10 10:37:57 +1000

    Testing bounced and stacked at the same time.

--- src/test/js/atomic/tests/stacked/Future/FutureParTest.js
+++ src/test/js/atomic/tests/stacked/Future/FutureParTest.js
@@ -1,7 +1,9 @@
 require("../../../include/include.js");
 
-var testParOverConstant = forEachWithSpy_(testArrays, function(array, spy) {
-    var futures = array.map(Future.constant);
-    Future.par(futures)(spy);
-    spy.verifyArgs([[array]]);
+var test = unitTest(function(Future, Async, Bfuture, Bsync) {
+
+    testArrays.forEach(function(array) {
+        var futures = array.map(Future.constant);
+        checkF(Future.par(futures), array);
+    });
 });
