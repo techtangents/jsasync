8a15436b6f8176bbcd8127ae70c279a6baae03d1
techtangents <dev@ephox.com>
2011-07-12 19:55:47 +1000

    Moving to the new way of testing.

--- src/test/js/atomic/tests/stacked/Bsync/BsyncConstantTest.js
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncConstantTest.js
@@ -1,6 +1,8 @@
 require("../../../include/include.js");
 
-var test = forEach2WithSpy_(testInts, testInts, function(ignored, input, spy) {
-    checkBfPass(Bsync.constant(input)(ignored), input);
-    checkBfFail(Bsync.constantFail(input)(ignored), input);
+var test = unitTest(function(Future, Async, Bfuture, Bsync) {
+    forEach2WithSpy(testInts, testInts, function(ignored, input, spy) {
+        checkBfPass(Bsync.constant(input)(ignored), input);
+        checkBfFail(Bsync.constantFail(input)(ignored), input);
+    });
 });
