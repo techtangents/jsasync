5ed6382e36764d89838d3d33eb3702fcc5d1f9e6
techtangents <dev@ephox.com>
2011-07-12 19:48:02 +1000

    Moving to the new way of testing.

--- src/test/js/atomic/tests/stacked/Bsync/BsyncAmapTest.js
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncAmapTest.js
@@ -4,19 +4,20 @@ var test = unitTest(function(Future, Async, Bfuture, Bsync) {
     forEach2WithSpy(testFunctions, testArrays, function(f, inputs, spy) {
         checkBfPass(Bsync.sync(f).amap(inputs), inputs.map(f));
     });
-});
 
-var testFail = forEachWithSpy_(testValues, function(inputs_, spy) {
-    var inputs = [1].concat(inputs_); // so there's always a fail
-    var pred = gte2;
+    forEachWithSpy(testValues, function(inputs_, spy) {
+        var inputs = [1].concat(inputs_); // so there's always a fail
+        var pred = gte2;
 
-    var expected = sift(inputs, pred);
+        var expected = sift(inputs, pred);
 
-    var bs = Bsync.bsync(function(a, passCb, failCb) {
-         pred(a) ? passCb(a) : failCb(a);
+        var bs = Bsync.bsync(function(a, passCb, failCb) {
+             pred(a) ? passCb(a) : failCb(a);
+        });
+
+        bs.amap(inputs)(explode, spy);
+        waitForSpy(spy);
+        var actual = spy.getInvocationArgs()[0][0];
+        assertArrayOfEitherEquals(expected, actual);
     });
-
-    bs.amap(inputs)(explode, spy);
-    var actual = spy.getInvocationArgs()[0][0];
-    assertArrayOfEitherEquals(expected, actual);
 });
