da832ce47543d68ba0f036c3422907132391d92e
techtangents <dev@ephox.com>
2011-07-12 14:04:41 +1000

    Moving to the new way of testing.

--- src/test/js/atomic/tests/stacked/Bsync/BsyncAlwaysTest.js
+++ src/test/js/atomic/tests/stacked/Bsync/BsyncAlwaysTest.js
@@ -1,27 +1,32 @@
 require("../../../include/include.js");
 
-var testPass = forEach_(testValues, function(input) {
-    var check = function(bs) {
-        checkBfPass(bs(input), input);
+var test = unitTest(function(Future, Async, Bfuture, Bsync) {
+
+    var queck = function(f, input) {
+        return function(bs) {
+            f(bs(input), input);
+        };
     };
 
-    check(Bsync.identity.alwaysPass());
-    check(Bsync.constant(input).alwaysPass());
-    check(Bsync.identity[">>>"](Bsync.constant(input)).alwaysPass());
-    check(Bsync.identity[">>>"](Bsync.constant(input).alwaysPass()));
-    check(Bsync.constantFail(input).alwaysPass());
-    check(Bsync.faildentity.alwaysPass());
-});
+    forEach(testValues, function(input) {
+        var check = queck(checkBfPass, input);
 
-var testFail = forEach_(testValues, function(input) {
-    var check = function(bs) {
-        checkBfFail(bs(input), input);
-    };
+        check(Bsync.identity.alwaysPass());
+        check(Bsync.constant(input).alwaysPass());
+        check(Bsync.identity[">>>"](Bsync.constant(input)).alwaysPass());
+        check(Bsync.identity[">>>"](Bsync.constant(input).alwaysPass()));
+        check(Bsync.constantFail(input).alwaysPass());
+        check(Bsync.faildentity.alwaysPass());
+    });
 
-    check(Bsync.identity.alwaysFail());
-    check(Bsync.constant(input).alwaysFail());
-    check(Bsync.identity[">>>"](Bsync.constant(input)).alwaysFail());
-    check(Bsync.identity[">>>"](Bsync.constant(input).alwaysFail()));
-    check(Bsync.constantFail(input).alwaysFail());
-    check(Bsync.faildentity.alwaysFail());
+    forEach(testValues, function(input) {
+        var check = queck(checkBfFail, input);
+
+        check(Bsync.identity.alwaysFail());
+        check(Bsync.constant(input).alwaysFail());
+        check(Bsync.identity[">>>"](Bsync.constant(input)).alwaysFail());
+        check(Bsync.identity[">>>"](Bsync.constant(input).alwaysFail()));
+        check(Bsync.constantFail(input).alwaysFail());
+        check(Bsync.faildentity.alwaysFail());
+    });
 });
